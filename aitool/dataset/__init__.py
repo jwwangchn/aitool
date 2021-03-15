c1f20232091d7296764dbba7207353e66d84c079
jwwangchn <jwwangchn@outlook.com>
2021-03-15 19:09:16 +0800

    add confusion matrix calculation codes

--- aitool/dataset/__init__.py
+++ aitool/dataset/__init__.py
@@ -1,6 +1,7 @@
-from .parse import PklParserBase, PklParserMask, PklParserMaskOBB, COCOParser, xml_parser_plane, xml_parser_rovoc
+from .parse import PklParserBase, PklParserMask, PklParserMaskOBB, COCOParser, COCOParserTinyPerson, COCOJsonResultParser, xml_parser_plane, xml_parser_rovoc
 from .dump import XMLDumperBase, XMLDumperPlane, XMLDumperRoVOC, ObjectDumperBase, ObjectDumperPlane, TXTDumperBase, TXTDumperBase_HJJ_Ship
 from .convert2coco import Convert2COCO
 from .statistic import COCOStatisticBase, COCOStatistic_Plane
+from .utils import get_confusion_matrix_indexes
 
-__all__ = ['PklParserBase', 'PklParserMask', 'PklParserMaskOBB', 'XMLDumperBase', 'XMLDumperPlane', 'XMLDumperRoVOC', 'COCOParser', 'Convert2COCO', 'COCOStatisticBase', 'COCOStatistic_Plane', 'ObjectDumperBase', 'ObjectDumperPlane', 'xml_parser_plane', 'xml_parser_rovoc', 'TXTDumperBase', 'TXTDumperBase_HJJ_Ship']
+__all__ = ['PklParserBase', 'PklParserMask', 'PklParserMaskOBB', 'XMLDumperBase', 'XMLDumperPlane', 'XMLDumperRoVOC', 'COCOParser', 'COCOParserTinyPerson', 'Convert2COCO', 'COCOStatisticBase', 'COCOStatistic_Plane', 'ObjectDumperBase', 'ObjectDumperPlane', 'xml_parser_plane', 'xml_parser_rovoc', 'TXTDumperBase', 'TXTDumperBase_HJJ_Ship', 'COCOJsonResultParser', 'get_confusion_matrix_indexes']
