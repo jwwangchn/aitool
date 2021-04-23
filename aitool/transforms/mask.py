f2df52c712e271f406a98088eee8eb7f2c529178
jwwangchn <jwwangchn@outlook.com>
2021-04-23 21:54:48 +0800

    add mask2bbox

--- aitool/transforms/mask.py
+++ aitool/transforms/mask.py
@@ -27,3 +27,16 @@ def mask2polygon(mask):
     polygon = Polygon(mask_coord)
 
     return polygon
+
+def mask2bbox(mask):
+    """convert mask to bbox
+
+    Arguments:
+        mask {list} -- contains coordinates of mask boundary ([x1, y1, x2, y2, ...])
+    """
+    mask_x = mask[0::2]
+    mask_y = mask[1::2]
+    xmin, ymin, xmax, ymax = min(mask_x), min(mask_y), max(mask_x), max(mask_y)
+    bbox = [xmin, ymin, xmax, ymax]
+
+    return bbox
